83d43bfce000715c6ddeb11b95b1be97e0fa8fa3
Jon Orton <jonorton66@gmail.com>
2018-01-07 13:57:34 -0500

    testing cargo new

---
+++ Test/target/debug/Test.d
@@ -0,0 +1,1 @@
+/home/jorton/Dev/rust/Test/target/debug/Test: /home/jorton/Dev/rust/Test/src/lib.rs /home/jorton/Dev/rust/Test/src/main.rs
---
+++ Test/target/debug/libTest.d
@@ -0,0 +1,1 @@
+/home/jorton/Dev/rust/Test/target/debug/libTest.rlib: /home/jorton/Dev/rust/Test/src/lib.rs
